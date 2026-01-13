91d1306b7dc98d38785d1fc8ec680152e9c7da2d
Damien-16 <pv77ngi@eduvaud.ch>
2026-01-13 17:08:16 +0100

    feat(ex) : L8

--- main.js
+++ main.js
@@ -3,27 +3,40 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             sizes: ['S', 'M', 'L', 'XL'],
             variants: [
-                {id: 2234, color: 'green'},
-                {id: 2235, color: 'blue'}
-            ]
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ],
+            onSale: true
         }
     },
     methods: {
         addToCart(){
             this.cart +=1
         },
-        clearToCart(){
-            if (this.cart >= 1) {
-                this.cart -= 1
-            }
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
         },
-        updateImage(variantImage){
-            this.image = variantImage
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' is on sale.'
+            }
+            return ''
         }
     }
-});
+})
